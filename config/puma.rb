a58c956334fa539f40b1a4435534b9104723ab57
Nikolay Rys <nikolay@rys.me>
2019-10-07 04:20:29 +0200

    fix port

--- config/puma.rb
+++ config/puma.rb
@@ -7,14 +7,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 2 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
 if ENV.fetch("RAILS_ENV") == 'production'
   bind "unix:#{Rails.root}/tmp/sockets/puma.sock"
 else
-  port 3000
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port ENV.fetch("PORT") { 3000 }
 end
 
 # Specifies the `environment` that Puma will run in.
