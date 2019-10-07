bac99c75b91358431592e3d4366091eecc66dc86
Nikolay Rys <nikolay@rys.me>
2019-10-07 08:53:39 +0200

    use proper gem for test DB reset

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'database_cleaner'
 
 class ActiveSupport::TestCase
-  (ActiveRecord::Base.connection.tables - %w{schema_migrations}).each do |table_name|
-    ActiveRecord::Base.connection.execute "DELETE FROM #{table_name};"
+  DatabaseCleaner.strategy = :deletion
+  def self.teardown
+    DatabaseCleaner.clean
   end
 end
