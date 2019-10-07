0560eaaddc00d8b697989b9d1f59b1cd8198b747
Nikolay Rys <nikolay@rys.me>
2019-10-07 10:03:52 +0200

    add transfer validation tests

--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class TransferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid without participants' do
+    assert_not Transfer.new(amount: 10).valid?
+  end
+
+  test 'invalid with negative amount' do
+    participant = User.create!(email: 'first@email.com', password: 'secret', balance: 10)
+    assert_not Transfer.new(sender: participant, amount: -5).valid?
+  end
 end
