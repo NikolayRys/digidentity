d09fc72ec10d53464fbf788d359a6ec524ad983f
Nikolay Rys <nikolay@rys.me>
2019-10-07 16:11:14 +0200

    add money transfer interface

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,10 +5,19 @@ class TransactionsController < ApplicationController
 
   def index
     @user = current_user
+    @other_users = User.all.where.not(id: current_user.id)
   end
 
   def create
-
+    amount = params[:amount].to_i
+    if current_user.can_transfer?(amount)
+      receiver = User.find(params[:receiver_id])
+      current_user.send_money_to(receiver, amount, params[:note])
+      flash.notice = 'Transfer was successful'
+    else
+      flash.alert = 'Transfer was not successful: amount is too large or negative.'
+    end
+    redirect_to '/transactions'
   end
 end
 
